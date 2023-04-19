bbc67fcf8bb938bde173296c4e4dc8b714e34585
abdullojr <iamnozimjon@gmail.com>
2023-04-19 18:01:31 +0500

    +

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import mysql from 'mysql'
 import Router from './routes/router'
 import path from 'path'
-import { env } from 'process'
+import http from 'http'
+import { Server } from 'socket.io'
 
 dotenv.config({ path: path.resolve(__dirname, '..', '.env') })
 
 const App = express()
+const server = http.createServer(App)
+const io = new Server(server)
 
 App.use(express.json({ limit: '10mb' }))
 App.use(express.urlencoded({ extended: true }))
@@ -31,6 +34,19 @@ con.getConnection(err => {
   } else {
     console.log('Connection established!')
   }
+})
+
+io.on('connection', socket => {
+  console.log('A user connected!')
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected!')
+  })
+})
+
+const port = 8000
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
 })
 
 export { con }
